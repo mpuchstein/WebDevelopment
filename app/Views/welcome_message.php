bc580341a8aec92bca5ca3d457e03ba5183da80a
Viktoria <s4vischm@uni-trier.de>
2024-11-11 16:31:15 +0100

    Text rückgängig gemacht (A) hinzugefügt

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -237,9 +237,9 @@
 
 <section>
 
-    <h1>About this page /A</h1>
+    <h1>About this page</h1>
 
-    <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    <p>(A) The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
     <p>If you would like to edit this page you will find it located at:</p>
 
