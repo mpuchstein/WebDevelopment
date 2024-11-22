a5d7ccca03d7caef363e6446d5dd2e9541e1d9e1
Viktoria <s4vischm@uni-trier.de>
2024-11-22 12:28:33 +0100

    möglicherweise Interessant

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,20 @@
+<footer class="footer mt-auto py-3 bg-footer" >
+    <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
+
+<nav
+
+<main class="flex-shrink-0">
+    <div class="card">
+        <div class ="card-header">
+            Überschrift
+        </div>
+        <div class ="card-body">
+
+        </div>
+    </div>
+</main>
+
+<body class="d-flex flex-column h-100">
