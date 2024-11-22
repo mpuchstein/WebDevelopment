12436ea4e2ad320cca21682fb32972a9a6226ede
mpuchstein <matthias@puchstein.bayern>
2024-11-22 12:17:32 +0100

    Übungsinhalt

---
+++ app/Views/body.php
@@ -0,0 +1,10 @@
+<main class="flex-shrink-0 mt-2 mb-2">
+    <div class="card h-100">
+        <div class="card-header">
+            Überschrift
+        </div>
+        <div class="">
+            Item
+        </div>
+    </div>
+</main>
---
+++ app/Views/template/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer mt-auto py-3 bg-footer text-white">
+    <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
+<!-- SCRIPTS -->
+
+<!-- -->
+
+</body>
+</html>
