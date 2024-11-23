2e925330a73060e5a2610f833e3ed2efe8252547
mpuchstein <matthias@puchstein.bayern>
2024-11-23 03:20:54 +0100

    made the footer a little bit fancy

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,10 +1,14 @@
-<footer class="footer sticky-bottom container-fluid mt-auto py-3 bg-footer text-light pb-3 mt-2">
-    <div class="container">
-        <span class="text-white ft">©Web-Entwicklung Team 2024</span>
-        <!-- TODO: figure out why index.php is necessary in links -->
-        <a href="<?=base_url('index.php/impressum')?>">Impressum</a>
-        <span class="text-white ft">Datenschutz</span>
-        <span class="text-white ft">Kontakt</span>
+<footer class="footer sticky-bottom container-fluid mt-auto bg-footer text-light pb-1 mt-2">
+    <div class="row">
+        <div class="col text-start ps-3">
+            ©Web-Entwicklung Team 2024
+        </div>
+        <div class="col text-end pe-3">
+            <!-- TODO: figure out why index.php is necessary in links -->
+            <a href="<?=base_url('index.php/impressum')?>">Impressum</a>
+            <a href="<?=base_url('index.php/datenschutz')?>">Datenschutz</a>
+            <a href="<?=base_url('index.php/kontakt')?>">Kontakt</a>
+        </div>
     </div>
 </footer>
 <!-- body and html are opened in nav/head and need to be closed here -->
