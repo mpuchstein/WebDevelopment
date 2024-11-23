22dba61f1ddc2c5009ae476f783de9dd477dc2dd
mpuchstein <matthias@puchstein.bayern>
2024-11-23 02:16:40 +0100

    made the logo clickable to come back home

--- app/Views/template/nav.php
+++ app/Views/template/nav.php
@@ -3,7 +3,9 @@
 <!-- HEADER: MENU + HEROE SECTION -->
 <header>
     <div class="container bg-header pt-3 pb-3 mt-2">
-        <img src="<?= base_url('assets/images/07_-_WE-Logo.svg')?>" alt="Webentwicklung" width="125"  class="logo"/>
+        <a href="<?=base_url('/')?>">
+            <img src="<?= base_url('assets/images/07_-_WE-Logo.svg')?>" alt="Webentwicklung" style="width: 128px;height: 64px;"  class="logo"/>
+        </a>
     </div>
     <nav class="navbar container bg-header text-white pt-3 pb-3 mt-2">
         Tasks | Boards | Spalten
