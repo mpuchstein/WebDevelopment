26a3659746b71e3237c4b528b2acc59d3b66ad92
mpuchstein <matthias@puchstein.bayern>
2024-11-23 03:22:45 +0100

    added todo to routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,5 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+/**
+ * TODO: Add routes for all href
+ */
 $routes->get('/', 'Home::index');
 $routes->get('impressum', 'Impressum::index');
