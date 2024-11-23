6d1b4012569ac1ad1646da011235ae516b81a8a1
mpuchstein <matthias@puchstein.bayern>
2024-11-23 02:42:06 +0100

    some more experiments

--- app/Views/template/nav.php
+++ app/Views/template/nav.php
@@ -1,13 +1,13 @@
 <body class="d-flex flex-column h-100">
 
 <!-- HEADER: MENU + HEROE SECTION -->
-<header class="sticky-top container-fluid">
-    <div class="bg-header pt-3 pb-3 mt-2">
+<header class="accordion-header bg-black sticky-top">
+    <div class="bg-header pb-3 mt-2">
         <a href="<?=base_url('/')?>">
             <img src="<?= base_url('assets/images/07_-_WE-Logo.svg')?>" alt="Webentwicklung" style="width: 128px;height: 64px;"  class="logo"/>
         </a>
     </div>
-    <nav class="navbar bg-header text-white pt-3 pb-3 mt-2">
+    <nav class="navbar bg-header text-white pt-3 pb-3 mt-2 mb-2">
         Tasks | Boards | Spalten
     </nav>
 </header>
