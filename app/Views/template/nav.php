02f2c3ffdc2fb642e81837e6fd36342e251b4b8e
mpuchstein <matthias@puchstein.bayern>
2024-11-23 03:09:07 +0100

    made the navbar a little bit fancy

--- app/Views/template/nav.php
+++ app/Views/template/nav.php
@@ -2,12 +2,14 @@
 
 <!-- HEADER: MENU + HEROE SECTION -->
 <header class="accordion-header bg-black sticky-top">
-    <div class="bg-header pb-3 mt-2">
+    <div class="bg-header m-1 p-1 ps-3">
         <a href="<?=base_url('/')?>">
             <img src="<?= base_url('assets/images/07_-_WE-Logo.svg')?>" alt="Webentwicklung" style="width: 128px;height: 64px;"  class="logo"/>
         </a>
     </div>
-    <nav class="navbar bg-header text-white pt-3 pb-3 mt-2 mb-2">
-        Tasks | Boards | Spalten
+    <nav class="navbar-toggler bg-light-subtle m-1 p-2 ps-3">
+        <a href="<?=base_url('index.php/tasks')?>" class="btn btn-outline-dark">Tasks</a>
+        <a href="<?=base_url('index.php/tasks')?>" class="btn btn-outline-dark">Boards</a>
+        <a href="<?=base_url('index.php/tasks')?>" class="btn btn-outline-dark">Spalten</a>
     </nav>
 </header>
