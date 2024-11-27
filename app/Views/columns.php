2026a9ee6504c124d6f5c60a6f3f85b14130648e
Viktoria <s4vischm@uni-trier.de>
2024-11-27 13:27:57 +0100

    Labels als Floating Labels (vorherige wurden nur ausgeklammert) und Spalten wurden an Taskgestaltung angepasst

--- app/Views/columns.php
+++ app/Views/columns.php
@@ -1,16 +1,16 @@
 <main class="container mt-2">
     <div class="card">
-        <div class="card-header">
+        <div class="card-header bg-black text-light">
             <h1>Spalten</h1>
         </div>
-        <div class="card-body">
+        <div class="card-body bg-dark text-light">
 <!--            <button type="button" class="" onclick="location.href='<?=base_url('index.php/spalten/formular')?>'">Erstellen</button>
 -->
-            <a href="<?=base_url('index.php/spalten/formular')?>" class="btn btn-outline-dark mb-2">Erstellen</a>
+            <a href="<?=base_url('index.php/spalten/formular')?>" class="btn btn-outline-light mb-2">Erstellen</a>
 <!--        </div>
         <div class="card-footer">
 -->
-            <table class="table table-bordered">
+            <table class="table table-bordered rounded">
                 <tr>
                     <th>ID</th>
                     <th>Board</th>
