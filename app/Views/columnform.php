dffb4326ea9c654b4702e57f837c530bf5aac0ba
Viktoria <s4vischm@uni-trier.de>
2024-11-30 15:26:46 +0100

    worked on the labels - not finished

--- app/Views/columnform.php
+++ app/Views/columnform.php
@@ -3,29 +3,29 @@
         <div class="card-header bg-black text-light border-primary">
             <h1>Spalte erstellen</h1>
         </div>
-        <div class="card-body bg-dark">
+        <div class="card-body bg-dark text-light">
             <form class="">
                 <div class="form-floating mb-3">
                     <!--<label for="columnlabel" class="col-3">Spaltenbezeichnung:</label>-->
                     <!--<input type="text" id="columnlabel" class="col-4">-->
-                    <input type="text" id="columnlabel" class="form-control" placeholder="Spaltenbezeichnung">
+                    <input type="text" id="columnlabel" class="form-control bg-black text-white" placeholder="Spaltenbezeichnung">
                     <label for="columnlabel">Spaltenbezeichnung</label>
                 </div>
                 <div class="form-floating mb-3">
                     <!--<label for="columndesc" class="col-3">Spaltenbeschreibung:</label>-->
                     <!--<textarea id="columndesc" class="col-4"></textarea>-->
-                    <textarea id="columndesc" class="form-control" placeholder="Spaltenbeschreibung" style="height: 100px"></textarea>
+                    <textarea id="columndesc" class="form-control bg-black text-white" placeholder="Spaltenbeschreibung" style="height: 100px"></textarea>
                     <label for="columndesc">Spaltenbeschreibung</label>
                 </div>
                 <div class="form-floating mb-3">
                     <!--<label for="sortid" class="col-3">Spaltenbezeichnung:</label>-->
                     <!--<input type="text" id="sortid" class="col-4">-->
-                    <input type="text" id="sortid" class="form-control" placeholder="Sortier-ID">
+                    <input type="text" id="sortid" class="form-control bg-black text-white" placeholder="Sortier-ID">
                     <label for="sortid">Sortier-ID</label>
                 </div>
                 <div class="row">
                     <label for="board" class="col-3 text-light rounded">Board auswählen:</label>
-                    <select id="board" name="board" class="col-4">
+                    <select id="board" name="board" class="col-4 bg-black text-white">
                         <option value="general">Allgemeine Todos</option>
                         <option value="special">Spezielle Todos</option>
                     </select>
