42f3416c84cccb8bcf7fe8bb7c6a52a26434514b
Viktoria <s4vischm@uni-trier.de>
2025-01-06 23:56:19 +0100

    task.php

--- app/Views/tasks.php
+++ app/Views/tasks.php
@@ -1,7 +1,7 @@
 <!-- Future Kanban board from task 1 -->
 <main class="container-fluid mt-2 mb-2 p-3">
     <div class="card h-100 border-primary">
-        <div class="card-header  border-primary bg-black"> <!-- https://dozent.wi1we.uni-trier.de/public/personen/index_val-->
+        <div class="card-header  border-primary bg-black">
             <h1>Tasks</h1> <!-- Headline -->
         </div>
         <div class="card-body  border-primary">
