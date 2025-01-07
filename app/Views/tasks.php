aadbd174d7e04c16c1b99d47988d72bb84b0d5c9
Viktoria <s4vischm@uni-trier.de>
2025-01-07 20:15:48 +0100

    task.php updatet

--- app/Views/tasks.php
+++ app/Views/tasks.php
@@ -1,51 +1,54 @@
-<!-- Future Kanban board from task 1 -->
 <main class="container-fluid mt-2 mb-2 p-3">
     <div class="card h-100 border-primary">
-        <div class="card-header  border-primary bg-black">
-            <h1>Tasks</h1> <!-- Headline -->
+        <div class="card-header border-primary bg-black">
+            <h1><?= esc($headline) ?></h1> <!-- Headline -->
         </div>
-        <div class="card-body  border-primary">
+        <div class="card-body border-primary">
             <a href="<?=base_url('tasks/form')?>" class="btn btn-primary">Neu</a>
         </div>
-        <div class="card-body table-responsive">
-            <table class="table table-dark table-striped">
+        <div class="card-body bg-dark text-light border-primary table-responsive">
+            <p></p>
+            <table class="table table-dark table-striped table-bordered w-100">
                 <thead>
                 <tr>
-                    <th scope="col">Personid</th>
-                    <th scope="col">spaltenid</th>
-                    <th scope="col">taskid</th>
-                    <th scope="col">sortid</th>
-                    <th scope="col">task</th>
-                    <th scope="col">notizen</th>
-                    <th scope="col">erstelldatum</th>
-                    <th scope="col">erinnerung</th>
-                    <th scope="col">deadline</th>
-                    <th scope="col">erledigt</th>
-                    <th scope="col">geloescht</th>
-                    <th>Bearbeiten</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">PersonenID</th>
+                    <th scope="col">SpaltenID</th>
+                    <th scope="col">TaskartenID</th>
+                    <th scope="col">SortID</th>
+                    <th scope="col">Task</th>
+                    <th scope="col">Notizen</th>
+                    <th scope="col">Erstelldatum</th>
+                    <th scope="col">Erinnerungsdatum</th>
+                    <th scope="col">Erinnerung</th>
+                    <th scope="col">Deadline</th>
+                    <th scope="col">Erledigt</th>
+                    <th scope="col">Gelöschte</th>
+                    <th scope="col">Bearbeiten</th>
                 </tr>
                 </thead>
                 <tbody>
-                   <tr>
-                       <!-- one row -->
-                       <td>1</td>
-                       <td>2</td>
-                       <td>1</td>
-                       <td>1</td>
-                       <td>100</td>
-                       <td>Folien für Vortrag</td>
-                       <td></td>
-                       <td>2025-01-06 00:00:00</td>
-                       <td>1</td>
-                       <td>2025-01-15 00:00:00</td>
-                       <td>0</td>
-                       <td>0</td>
-                       <td>
-                       <td>
-                           <a href="<?=base_url('tasks/form/')?>" class="fa-solid fa-pen-to-square" title="Bearbeiten"></a>
-                           <a href="<?=base_url('tasks/delete/')?>" class="fa-solid fa-eraser" title="Löschen"></a>
-                       </td>
-                   </tr>
+                <?php foreach ($tasks as $task): ?>
+                    <tr>
+                        <td><?= esc($task['id']) ?></td>
+                        <td><?= esc($task['personenid']) ?></td>
+                        <td><?= esc($task['spaltenid']) ?></td>
+                        <td><?= esc($task['taskartenid']) ?></td>
+                        <td><?= esc($task['sortid']) ?></td>
+                        <td><?= esc($task['task']) ?></td>
+                        <td><?= esc($task['notizen']) ?></td>
+                        <td><?= esc($task['erstelldatum']) ?></td>
+                        <td><?= esc($task['erinnerungsdatum']) ?></td>
+                        <td><?= esc($task['erinnerung']) ?></td>
+                        <td><?= esc($task['deadline']) ?></td>
+                        <td><?= esc($task['erledigt']) ?></td>
+                        <td><?= esc($task['geloeschte']) ?></td>
+                        <td>
+                            <a href="<?=base_url('tasks/form/' . esc($task['id']))?>" class="fa-solid fa-pen-to-square" title="Bearbeiten"></a>
+                            <a href="<?=base_url('tasks/delete/' . esc($task['id']))?>" class="fa-solid fa-eraser" title="Löschen"></a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
