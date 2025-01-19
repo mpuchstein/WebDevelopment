5eeb478116a3defeb7bbb4c17476137668b1e381
Viktoria <s4vischm@uni-trier.de>
2025-01-19 22:41:21 +0100

    SpaltenModel (TaskModel angepasst)

--- app/Models/SpaltenModel.php
+++ app/Models/SpaltenModel.php
@@ -4,21 +4,60 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class SpaltenModel extends Model {
+    protected $table = 'spalten';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['boardsid', 'sortid', 'spalte', 'spaltenbeschreibung', 'geloescht'];
 
+    // getAllSpalten returns all tasks ordered by task
+    public function getAllSpalten(int $id = null): array //return Spalten
+    {
+        $query = $this->db->table($this->table)
+            ->select("*")
+            ->orderBy('spalte', 'ASC');
+        if ($id !== null) {
+            $query->where("id", $id);
+            return $query->get()->getRowArray();
+        }
+        return $query->get()->getResultArray();
 
-  /*  public function getSpalten($id) {
-        $this->spalten = $this->db->table('spalten');
-        $this->spalten->select('*');
+    }
 
-        IF ($id != NULL)
-            $this->spalten->where('spalten.id', $id);
+    // getActiveSpalten returns all tasks that are not deleted
+    public function getActiveSpalten(int $id = null): array{
+        $query = $this->db->table($this->table)
+            ->select("*")
+            ->where('geloescht', 0)
+            ->orderBy('spalte', 'ASC');
+        if ($id !== null) {
+            $query->where("id", $id);
+            return $query->get()->getRowArray();
+        }
+        return $query->get()->getResultArray();
+    }
 
-        $result = $this->spalten->get();
+    // insertSpalten adds a new spalten into the database, values are given by $data
+    // return the $id
+    public function insertSpalten(array $data): int
+    {
+        $this->db->table($this->table)->insert($data);
+        return $this-> db -> insertID();
+    }
 
-        if ($id != NULL)
-            return $result->getRowArray();
-        else
-            return $result->getResultArray();
-    } */
+    // updateSpalten updates an existing Spalten identified by $id with the new $data
+    public function updateSpalten(int $id, array $data): bool
+    {
+        return $this->db->table($this->table)->update($data, ['id' => $id]);
+    }
 
+    // deletes and removes a spalten identified by $id unrecoverably and permanently
+    public function deleteSpalten(int $id): bool
+    {
+        return $this->db->table($this->table)->delete(['id' => $id]);
+    }
+
+    // deletes a spalten identified by $id but does not remove it from the database
+    public function setSpaltenDeleted(int $id): bool
+    {
+        return $this->db->table($this->table)->set(['geloescht' => 1])->where(['id' => $id])->update();
+    }
 }
