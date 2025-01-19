01c41350709639843e91f3441349ec22b6fe0246
Viktoria <s4vischm@uni-trier.de>
2025-01-19 01:37:35 +0100

    Spalten als Schabone wurde hinzugefügt

---
+++ app/Controllers/Spalten.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+class Spalten extends BaseController{
+
+  public function getindex(){
+      //Controller-Funktion, die alle erforderlichen Daten für die View zusammenstellt und
+      //an diese übergibt
+  }
+}
