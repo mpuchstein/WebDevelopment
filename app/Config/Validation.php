f689cb83069caf9d4d024f2875ce9fcaf08aa5f7
mpuchstein <matthias@puchstein.bayern>
2025-01-23 04:41:34 +0100

    added validation to columns, rewrote it serveral times :D

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -34,11 +34,56 @@ class Validation extends BaseConfig
      * @var array<string, string>
      */
     public array $templates = [
-        'list'   => 'CodeIgniter\Validation\Views\list',
+        'list' => 'CodeIgniter\Validation\Views\list',
         'single' => 'CodeIgniter\Validation\Views\single',
     ];
 
     // --------------------------------------------------------------------
     // Rules
     // --------------------------------------------------------------------
+
+    // Rules for validating arrays containing task data
+    public array $taskArray = [
+
+    ];
+
+    // Rules for validating arrays containing column data
+    public array $columnArray = [
+        'spalte' => [
+            'rules' => 'required',
+            'errors' => [
+                'required' => 'Bitte tragen Sie eine <strong>Bezeichnung</strong> ein.',
+            ],
+        ],
+        'spaltenbeschreibung' => [
+            'rules' => 'required',
+            'errors' => [
+                'required' => 'Bitte tragen Sie eine <strong>Beschreibung</strong> ein.',
+            ],
+        ],
+        'sortid' => [
+            'rules' => 'required|integer',
+            'errors' => [
+                'required' => 'Bitte tragen Sie eine <strong>SortID</strong> ein.',
+                'integer' => 'Bitte tragen Sie eine <strong>Zahl</strong> ein.',
+            ]
+        ],
+        'boardsid' => [
+            'rules' => 'required|is_not_unique[boards.id]',
+            'errors' => [
+                'required' => 'Bitte wählen Sie ein <strong>Board</strong> aus.',
+                'is_not_unique' => '<strong>Das gewählte Board existiert nicht!!</strong>',
+            ],
+        ],
+    ];
+    //rules for validating a delete action on columns
+    public array $columnDelete = [
+        'id' => [
+            'rules' => 'required|is_not_unique[spalten.id]',
+            'errors' => [
+                'required' => 'ID ist erforderlich, um eine Spalte zu löschen.',
+                'is_not_unique' => 'Diese ID existiert nicht. Kann also auch nicht gelöscht werden.',
+            ]
+        ]
+    ];
 }
--- app/Controllers/Columns.php
+++ app/Controllers/Columns.php
@@ -2,29 +2,10 @@
 
 namespace App\Controllers;
 
-use old\Spalten;
 use App\Models\Database;
+
 class Columns extends BaseController
 {
-    // Validierungsregeln für die Bearbeitung von Spalten
-    public $spaltenbearbeiten = [
-        'spalte' => 'required',
-        'spaltenbeschreibung' => 'required',
-        'sortid' => 'integer',
-    ];
-
-    //Fehlermeldungen für die Validierung von Spalten
-    public $spaltenbearbeiten_errors = [
-        'spalte' => [
-            'required' => 'Bitte tragen Sie einen Spaltebezeichnung ein.',
-        ],
-        'spaltenbeschreibung' => [
-            'required' => 'Bitte tragen Sie eine Beschreibung ein.',
-        ],
-        'sortid' => [
-            'integer' => 'Die Sortid muss eine Zahl sein.',
-        ],
-    ];
     public function getIndex()
     {
         $database = new Database();
@@ -35,22 +16,23 @@ class Columns extends BaseController
         echo view('template/footer');
     }
 
-    public function getCrud($type = 'new', $id = null){
+    public function getCrud($type = 'new', $id = null)
+    {
         $data['mode'] = $type;
         $database = new Database();
         $data['boards'] = $database->getBoards();
-        if($type == 'new' && $id == null){
-            $data['headline'] = 'New Column';
-        } elseif($type == 'edit' && $id != null){
-            $data['headline'] = 'Edit Column';
+        if ($type == 'new' && $id == null) {
+            $data['headline'] = 'Spalte erstellen';
+        } elseif ($type == 'edit' && $id != null) {
+            $data['headline'] = 'Spalte bearbeiten';
             $data['columns'] = $database->getColumns(columnId: $id)[0];
-        } elseif($type == 'delete' && $id != null){
-            $data['headline'] = 'Delete Column';
+        } elseif ($type == 'delete' && $id != null) {
+            $data['headline'] = 'Spalte löschen';
             $data['columns'] = $database->getColumns(columnId: $id)[0];
-        } elseif(($type == 'edit' || $type == 'delete') && $id == null) {
+        } elseif (($type == 'edit' || $type == 'delete') && $id == null) {
             return redirect()->to(base_url('/columns'));
-        } elseif($type == 'new' && $id != null){
-            return redirect()->to(base_url('/columns/crud/edit/'.$id));
+        } elseif ($type == 'new' && $id != null) {
+            return redirect()->to(base_url('/columns/crud/edit/' . $id));
         }
 //        echo('<pre>');
 //        var_dump($data);
@@ -62,49 +44,66 @@ class Columns extends BaseController
         echo view('template/footer');
     }
 
-    public function postNew(){
-        $data = $this -> request -> getPost();
-//        echo('<pre>');
-//        var_dump($data);
-//        echo('</pre>');
-//        die();
-        $database = new Database();
-        $id = $database->insertColumn($data);
-        return redirect()->to(base_url('columns'));
-    }
-
-    public function postEdit(){
-        $data = $this -> request -> getPost();
-//        echo('<pre>');
-//        var_dump($data);
-//        echo('</pre>');
-//        die();
-        $database = new Database();
-        $database->updateColumn($data['id'], $data);
-        return redirect()->to(base_url('columns'));
-    }
-
-    public function postDelete(){
-        $data = $this -> request -> getPost();
-//        echo('<pre>');
-//        var_dump($data);
-//        echo('</pre>');
-//        die();
-        $database = new Database();
-        $database->deleteColumn($data['id']);
-        return redirect()->to(base_url('columns'));
-    }
-
-    public function validateColumn($data)
+    public function postNew()
     {
-        // Validierung der Daten
-        if (!$this->validateData($data, $this->spaltenbearbeiten)) {
-            // Wenn die Validierung fehlschlägt, das Spaltenfomular mit Fehlermeldungen anzeigen
-            return view('dev/columnCrud', [
-                'errors' => $this->validator->getErrors(),
-            ]);
+        $data = $this->request->getPost();
+        $validation = service('validation');
+        if (!$validation->run($data, 'columnArray')) {
+            $database = new Database();
+            $errorData['headline'] = 'Spalte erstellen';
+            $errorData['mode'] = 'new';
+            $errorData['error'] = $validation->getErrors();
+            $errorData['boards'] = $database->getBoards();
+            $errorData['columns'] = $data;
+            echo view('template/header');
+            echo view('template/nav');
+            echo view('dev/columnCrud', $errorData);
+            echo view('template/footer');
+        } else {
+            $database = new Database();
+            $id = $database->insertColumn($data);
+            return redirect()->to(base_url('columns'));
         }
-        // Validierte Daten abrufen
-        return $this->validator->getValidated();
+    }
+
+    public function postEdit()
+    {
+        $data = $this->request->getPost();
+        $database = new Database();
+        $validation = service('validation');
+        if (!$validation->run($data, 'columnArray')) {
+            $errorData['headline'] = 'Spalte bearbeiten';
+            $errorData['mode'] = 'edit';
+            $errorData['error'] = $validation->getErrors();
+            $errorData['boards'] = $database->getBoards();
+            $errorData['columns'] = $data;
+            echo view('template/header');
+            echo view('template/nav');
+            echo view('dev/columnCrud', $errorData);
+            echo view('template/footer');
+        } else {
+            $database->updateColumn($data['id'], $data);
+            return redirect()->to(base_url('columns'));
+        }
+    }
+
+    public function postDelete()
+    {
+        $data = $this->request->getPost();
+        $database = new Database();
+        $validation = service('validation');
+        if (!$validation->run($data, 'columnDelete')) {
+            $data['columns'] = $database->getColumns(joinBoards: true);
+            echo view('template/header');
+            echo view('template/nav');
+            echo view('dev/columns', $data);
+            echo(
+                '<script>alert("'.$validation->getErrors()['id'].'")</script>'
+            );
+            echo view('template/footer');
+        } else {
+            $database->deleteColumn($data['id']);
+            return redirect()->to(base_url('columns'));
+        }
     }
 }
